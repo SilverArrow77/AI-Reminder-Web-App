fa1d98cee0d0d3994330ff5841f0a8de383ce9d9
Anshul Kalmegh <kalmeghanshul4@gmail.com>
2026-05-29 11:37:04 +0530

    wip

---
+++ app/(auth)/reset-password/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Router from 'next/router'
+import AuthLayout from '@/components/ui/AuthLayout'
+
+const resetPassword = () => {
+  return (
+    <div>resetPassword</div>
+  )
+}
+
+export default resetPassword
